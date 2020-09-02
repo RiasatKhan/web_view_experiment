e6d89ac8421ce821f8d9a22a3cf9cdcdf635b63b
Rajib <g.rajib23@gmail.com>
2020-09-02 14:06:09 +0600

    fixed file upload

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ Future main() async {
 
   await Permission.camera.request();
   await Permission.microphone.request();
-
+  await Permission.storage.request();
   runApp(MyApp());
 }
 
@@ -20,7 +20,10 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: InAppWebViewPage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: InAppWebViewPage(),
+    );
   }
 }
 
@@ -32,32 +35,132 @@ class InAppWebViewPage extends StatefulWidget {
 class _InAppWebViewPageState extends State<InAppWebViewPage> {
   InAppWebViewController _webViewController;
 
+  String url;
+
+  Future<bool> _onBack() async {
+    bool goBack;
+
+    var value =
+        await _webViewController.canGoBack(); // check webview can go back
+    print('print url->>>>>>>>>>>>>>>>>>>>>');
+    print(url);
+    //print (_webViewController.printCurrentPage());
+    if (url == 'https://doctor-dev.takemed.com.bd/' ||
+        url == 'https://doctor-dev.takemed.com.bd/portal') {
+      print('inside if');
+      setState(() {
+        goBack = false;
+      });
+      value = false;
+    }
+    if (value) {
+      _webViewController.goBack(); // perform webview back operation
+      return false;
+    } else {
+      await showDialog(
+        context: context,
+        builder: (context) => new AlertDialog(
+          title:
+              new Text('Confirmation ', style: TextStyle(color: Colors.purple)),
+          // Are you sure?
+          content: new Text('Do you want exit app ? '),
+          // Do you want to go back?
+          actions: <Widget>[
+            new FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+                setState(() {
+                  goBack = false;
+                });
+              },
+
+              child: new Text('NO'), // No
+            ),
+            new FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+
+                setState(() {
+                  goBack = true;
+                });
+              },
+
+              child: new Text('YES'), // Yes
+            ),
+          ],
+        ),
+      );
+
+      if (goBack) Navigator.pop(context); // If user press Yes pop the page
+
+      return goBack;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: WillPopScope(
+        onWillPop: _onBack,
+        child: Scaffold(
+          body: Container(
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    child: InAppWebView(
+                        initialUrl: "https://doctor-dev.takemed.com.bd/",
+                        initialOptions: InAppWebViewGroupOptions(
+                          crossPlatform: InAppWebViewOptions(
+                            mediaPlaybackRequiresUserGesture: false,
+                            debuggingEnabled: true,
+                          ),
+                        ),
+                        onWebViewCreated: (InAppWebViewController controller) {
+                          _webViewController = controller;
+                        },
+                        onLoadStart:
+                            (InAppWebViewController controller, String url) {
+                          setState(() {
+                            this.url = url;
+                          });
+                        },
+                        onLoadStop: (InAppWebViewController controller,
+                            String url) async {
+                          setState(() {
+                            this.url = url;
+                          });
+                        },
+                        androidOnPermissionRequest:
+                            (InAppWebViewController controller, String origin,
+                                List<String> resources) async {
+                          return PermissionRequestResponse(
+                              resources: resources,
+                              action: PermissionRequestResponseAction.GRANT);
+                        }),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            child: Column(children: <Widget>[
-      Expanded(
-        child: Container(
-          child: InAppWebView(
-              initialUrl: "https://doctor-dev.takemed.com.bd/",
-              initialOptions: InAppWebViewGroupOptions(
-                crossPlatform: InAppWebViewOptions(
-                  mediaPlaybackRequiresUserGesture: false,
-                  debuggingEnabled: true,
-                ),
-              ),
-              onWebViewCreated: (InAppWebViewController controller) {
-                _webViewController = controller;
-              },
-              androidOnPermissionRequest: (InAppWebViewController controller,
-                  String origin, List<String> resources) async {
-                return PermissionRequestResponse(
-                    resources: resources,
-                    action: PermissionRequestResponseAction.GRANT);
-              }),
+      appBar: AppBar(
+        title: Text("Second Page"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Hey,there!"),
         ),
       ),
-    ])));
+    );
   }
 }
